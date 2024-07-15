75b7217d239c35d20a87adb822ff9fc6dae3b669
laladeveloper <hafizsaqlainmujtaba@gmail.com>
2024-07-15 16:37:32 +0500

    cors update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,14 +19,34 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+// List of allowed origins
+const allowedOrigins = ["http://localhost:5173", "https://b2um.com"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if the origin is in the allowedOrigins list
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
+
+app.use(cors(corsOptions));
+
 app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
 app.use("/api/reviews", reviewRoute);
 app.use("/api/category", categoryRoute);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Saqlain your code is working");
